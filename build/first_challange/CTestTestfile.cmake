8bfc494f35173620004433f30029a6e830f85f08
Alexander Moser <moser.alexander@protonmail.com>
2022-12-02 19:47:02 +0100

    Inital commit

---
+++ build/first_challange/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexander/catkin_ws/src/first_challange
+# Build directory: /home/alexander/catkin_ws/build/first_challange
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
